d079506e28b7804d85f407fafea015bd7ce7f3c5
Nekosama20 <dangerouspig3@gmail.com>
2025-09-12 10:44:36 +0200

    Add files via upload

--- pykemon.py
+++ pykemon.py
@@ -1,9 +1,41 @@
-# IMPORT
+class pykemon:
 
-import json 
-import random 
+    def __init__(self, t, p, c, n, a, s, h):
+        self.type = t
+        self.pvb = p
+        self.pvc = c
+        self.nom = n
+        self.atk = a
+        self.spd = s
+        self.heal = h
 
-# LECTURE DU FICHIER JSON
-with open("data.json", "r") as f: 
-    data = json.load(f)
+    def attack(self, adv): 
+        print(f"{self.nom} attaque {adv.nom} avec {self.atk}")
+        adv.pv -= self.atk
+        
+    def heal(self, adv):
+            b = 1
+            m = 2
+            o = 3
+            r = 2
+            if self.type["type"] == "feu":
+                self.pvc += self.heal
+                if self.pvc > self.pvb:
+                     self.pvc = self.pvb
+                o -=1
+            elif self.type["type"] == "plante":
+                 self.atk += self.heal
+                 b -= 1
+            elif self.type["type"] == "eau":
+                adv.atk / 2 #pour un seul tour (à coder)
+                m -= 1
+            elif self.type["type"] == "normal":
+                 ##le pokémon adverse ne peut pas attaquer pendant ce tour
+                 r -= 1          
 
+    def weakness(self, adv):
+        ##à coder les faiblesses en fonction du type du pokémon
+
+    
+    def __repr__(self, adv): 
+        return f"{self.nom}: {self.pvc} // {adv.nom}: {adv.pvc}"
